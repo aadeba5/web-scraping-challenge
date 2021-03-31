8151b7194143afd7925f5df0380ff696b3bd9136
aadeba5 <74994708+aadeba5@users.noreply.github.com>
2021-03-31 02:21:46 -0500

    Flask App

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import scrape_mars
-import index.html
+from flask_pymongo import pymongo
+# import scrape_mars
+# import index.html
 
 
 app = Flask(__name__)
@@ -10,29 +10,50 @@ conn = 'mongodb://localhost:27017'
 client = pymongo.MongoClient(conn)
 
 db = client.mars_db
-collection = db.post
 
-# Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db_app"
-mongo = PyMongo(app, uri="mongodb://localhost:27017/scrape_app")
-# mongo = PyMongo(app)
+db.mars.drop()
 
-# Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
+db.mars.insert_many([
+        {   'title': 'news_title_text',
+            'news_p': 'news_p.text'},   
+            
+        {   'feature_image': 'feature_image'},
+            
+        {   'html_table': 'html_table'},   
+            
+        {   'hemisphere_image_urls': 'hemisphere_image_urls'
+        }])   
 
 
-@app.route("/")
+
+
+
+# # Use flask_pymongo to set up mongo connection
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db_app"
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/scrape_app")
+# # mongo = PyMongo(app)
+
+# # Or set inline
+# # mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
+
+
+# @app.route("/")
+# def index():
+#     scrape = mongo.db.scrape.find_one()
+#     return render_template("index.html", scrape=scrape)
+
+
+@app.route('/')
 def index():
-    scrape = mongo.db.scrape.find_many()
-    return render_template("index.html", scrape=scrape)
+    mars = list(db.mars.find())
+    print(mars)
 
+    return render_template('index.html', mars=mars)
 
-@app.route("/scrape")
-def scraper():
-    scrape = mongo.db.scrape
-    scrape_data = scrape_mars.scrape()
-    scrape.update({}, scrape_data, upsert=True)
-    return redirect("/", code=302)
+    # scrape = mongo.db.scrape
+    # scrape_data = mars_mission.scrape()
+    # scrape.update({}, scrape_data, upsert=True)
+    # return redirect("/", code=302)
 
 
 if __name__ == "__main__":
--- scrape_mars.py
+++ scrape_mars.py
@@ -329,6 +329,7 @@ client = pymongo.MongoClient(conn)
 
 # %%
 db = client.mars_db
+db.mars.drop()
 collection = db.post
 
 
@@ -392,63 +393,7 @@ hemisphere_image_urls = [
 # %%
 collection.insert_many(hemisphere_image_urls)
 
-
-# %%
-#!pip install flask_pymongo
-
-
-# %%
-# !pip install PyMongo
-
-
-# %%
-# from flask import Flask, render_template, redirect
-# from flask_pymongo import PyMongo
-# # import scrape_mars
-# import html
-
-
-# app = Flask(__name__)
-
-
-# %%
-# conn = 'mongodb://localhost:27017'
-# client = pymongo.MongoClient(conn)
-
-
-# %%
-# db = client.mars_db
-# collection = db.post
-
-
-# %%
-# # Use flask_pymongo to set up mongo connection
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db_app"
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/scrape_app")
-# # mongo = PyMongo(app)
-
-# # Or set inline
-# # mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
-
-
-# @app.route("/")
-# def index():
-#     scrape = mongo.db.scrape.find_many()
-#     return render_template("index.html", scrape=scrape)
-
-
-# @app.route("/scrape")
-# def scraper():
-#     scrape = mongo.db.scrape
-#     scrape_data = scrape_mars.scrape()
-#     scrape.update({}, scrape_data, upsert=True)
-#     return redirect("/", code=302)
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
+browser.quit()
 # %%
 
 
